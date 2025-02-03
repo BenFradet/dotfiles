b25bae9a49e6b30a54307fb368d2ddcc4582a229
benfradet <benjamin.fradet@gmail.com>
2025-02-03 10:09:59 +0100

    starship

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ cp -r $HOME/.config/sway $DIR
 cp -r $HOME/.config/tofi $DIR
 cp -r $HOME/.config/ghostty $DIR
 cp -r $HOME/.config/dunst $DIR
+cp -r $HOME/.config/starship.toml $DIR/starship
 
 cp $HOME/.zshrc $DIR/zsh/.zshrc
 sed -i '/^## DANGER ZONE/,/^## DANGER ZONE/d' $DIR/zsh/.zshrc
