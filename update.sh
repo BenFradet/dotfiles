a11bd2283beada040845e06bd18ab391d323169b
benfradet <benjamin.fradet@gmail.com>
2025-02-03 10:05:02 +0100

    dunst config

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ cp -r $HOME/.config/i3status-rust $DIR
 cp -r $HOME/.config/sway $DIR
 cp -r $HOME/.config/tofi $DIR
 cp -r $HOME/.config/ghostty $DIR
+cp -r $HOME/.config/dunst $DIR
 
 cp $HOME/.zshrc $DIR/zsh/.zshrc
 sed -i '/^## DANGER ZONE/,/^## DANGER ZONE/d' $DIR/zsh/.zshrc
