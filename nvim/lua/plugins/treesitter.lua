2f4054dc6ef05da78ac580b426e41a57813bed2a
benfradet <benjamin.fradet@gmail.com>
2025-11-02 13:07:34 +0100

    fix wgsl TS

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,14 +6,6 @@ return {
     "windwp/nvim-ts-autotag",
   },
   config = function()
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.wgsl = {
-      install_info = {
-        url = "https://github.com/szebniok/tree-sitter-wgsl",
-        files = {"src/parser.c"},
-      },
-    }
-
     local treesitter = require("nvim-treesitter.configs")
 
     treesitter.setup({
